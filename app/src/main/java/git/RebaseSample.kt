c6327dc4ff34c0362f05960d22c9a17f35937bd4
sunil_sc <sunil.sc@paytm.com>
2022-07-03 14:59:49 +0530

    rebase commit format

--- app/src/main/java/git/RebaseSample.kt
+++ app/src/main/java/git/RebaseSample.kt
@@ -2,7 +2,7 @@ package git
 
 class RebaseSample {
     fun method1(){
-        "This is the first commit for Rebase from Main"
-        "This is the third commit for Rebase from Main Branch"
+        "This is the first commit for Rebase from Main Branch"
+        "This is the fourth commit for Rebase from Main Branch"
     }
 }
--- app/src/main/java/git/RebaseSample2.kt
+++ app/src/main/java/git/RebaseSample2.kt
@@ -3,8 +3,6 @@ package git
 class RebaseSample2 {
     fun method1(){
         "This is the seond commit for Rebase from Main branch"
-
-
         "This is the third commit for Rebase from Main branch"
     }
 }
