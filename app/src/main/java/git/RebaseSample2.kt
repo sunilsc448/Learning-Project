9abb5d73beab2dcf8af7e9598e240b8f95af83c1
sunil_sc <sunil.sc@paytm.com>
2022-07-03 14:13:11 +0530

    Rebase 5 from Main

--- app/src/main/java/git/RebaseSample2.kt
+++ app/src/main/java/git/RebaseSample2.kt
@@ -3,5 +3,8 @@ package git
 class RebaseSample2 {
     fun method1(){
         "This is the seond commit for Rebase from Main branch"
+
+
+        "This is the third commit for Rebase from Main branch"
     }
 }
