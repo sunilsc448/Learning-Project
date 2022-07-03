087da7e9a4a3073bbac2dc1000c73877851bc2b8
sunil_sc <sunil.sc@paytm.com>
2022-07-03 14:50:20 +0530

    Rebase commit 6 from Main

--- app/src/main/java/git/RebaseSample.kt
+++ app/src/main/java/git/RebaseSample.kt
@@ -2,6 +2,7 @@ package git
 
 class RebaseSample {
     fun method1(){
-        "This is the first commit for Rebase"
+        "This is the first commit for Rebase from Main"
+        "This is the third commit for Rebase from Main Branch"
     }
 }
